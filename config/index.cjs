b09d6c16da7c59e7ac73ed5b3f90e513d1ba73af
amitions <94165105+amitions@users.noreply.github.com>
2023-05-17 10:41:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'c7d9da73375a48359074a10bb6f7735f',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'gBTziVl5o6D12kkyf2Pmh0qj8fISWbXFCmsX1IIo8WQ',
+      useTemplateId: '001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
