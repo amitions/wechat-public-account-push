a0d2f90f82772d636240c8208b167ea34bd32810
amitions <94165105+amitions@users.noreply.github.com>
2023-05-17 11:43:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '憨杨',
+      name: '憨',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o63hS6dKD0rAGnm9AiM4o3GIuMlU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'ES0oyHYbwbmop8ZSKocL48yClvTHVoMtvTQDH-qJIvk',
+      useTemplateId: 'A3WtUzKm7LDhWD1DsGkzTA_1fpKXF47if8I6uqWNBY0',
       province:'河南',
       city:'郑州',
     },
